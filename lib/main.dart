83d457c66ee01382bb0ded3d7b8e43afd657422f
Haldiant <nhalwidianto@gmail.com>
2024-07-04 15:53:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QuizzlerApp());
+}
+
+class QuizzlerApp extends StatelessWidget {
+  const QuizzlerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
